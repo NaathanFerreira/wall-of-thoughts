35acb1e4b4dbcab8f7f9de3abfa65b8b360c283b
Nathan <nathanferreira00@hotmail.com>
2024-01-29 10:42:25 -0300

    feat: form validations

--- src/app/components/thoughts/create-thought/create-thought.component.ts
+++ src/app/components/thoughts/create-thought/create-thought.component.ts
@@ -2,29 +2,53 @@ import { Component, OnInit } from '@angular/core';
 import { IThought } from '../thought';
 import { ThoughtService } from '../thought.service';
 import { Router } from '@angular/router';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-create-thought',
   templateUrl: './create-thought.component.html',
-  styleUrls: ['./create-thought.component.css']
+  styleUrls: ['./create-thought.component.css'],
 })
 export class CreateThoughtComponent implements OnInit {
+  form!: FormGroup;
 
-  thought: IThought = {
-    content: '',
-    author: '',
-    model: 'model1'
-  }
-
-  constructor(private thoughtService: ThoughtService, private router: Router) { }
+  constructor(
+    private thoughtService: ThoughtService,
+    private router: Router,
+    private formBuilder: FormBuilder
+  ) {}
 
   ngOnInit(): void {
+    this.form = this.formBuilder.group({
+      content: [
+        '',
+        Validators.compose([
+          Validators.required,
+          // don't allow empty spaces
+          Validators.pattern(/(.|\s)*\S(.|\s)*/),
+        ]),
+      ],
+      author: [
+        '',
+        Validators.compose([Validators.required, Validators.minLength(3)]),
+      ],
+      model: ['model1'],
+    });
   }
 
   createThought() {
-    this.thoughtService.create(this.thought).subscribe(() => {
-      this.router.navigate(['/listThoughts'])
-    })
+    console.log(this.form.get('author')?.errors);
+    if (this.form.valid) {
+      this.thoughtService.create(this.form.value).subscribe(() => {
+        this.router.navigate(['/listThoughts']);
+      });
+    }
   }
 
+  enableButton(): string {
+    if (this.form.valid) {
+      return 'botao';
+    }
+    return 'botao__desabilitado';
+  }
 }
--- src/app/components/thoughts/edit-thought/edit-thought.component.ts
+++ src/app/components/thoughts/edit-thought/edit-thought.component.ts
@@ -2,36 +2,60 @@ import { Component, OnInit } from '@angular/core';
 import { IThought } from '../thought';
 import { ThoughtService } from '../thought.service';
 import { ActivatedRoute, Router } from '@angular/router';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-edit-thought',
   templateUrl: './edit-thought.component.html',
-  styleUrls: ['./edit-thought.component.css']
+  styleUrls: ['./edit-thought.component.css'],
 })
 export class EditThoughtComponent implements OnInit {
+  form!: FormGroup;
 
-  thought: IThought = {
-    id: 0,
-    content: '',
-    author: '',
-    model: ''
-  }
-  constructor(private thoughtService: ThoughtService, private router: Router, private route: ActivatedRoute) { }
+  constructor(
+    private thoughtService: ThoughtService,
+    private router: Router,
+    private route: ActivatedRoute,
+    private formBuilder: FormBuilder
+  ) {}
 
   ngOnInit(): void {
-    const id = Number(this.route.snapshot.paramMap.get('id'))
+    const id = Number(this.route.snapshot.paramMap.get('id'));
 
     this.thoughtService.getById(id).subscribe((thought) => {
-      this.thought = thought
-    })
+      this.form = this.formBuilder.group({
+        id: [thought.id],
+        content: [
+          thought.content,
+          Validators.compose([
+            Validators.required,
+            // don't allow empty spaces
+            Validators.pattern(/(.|\s)*\S(.|\s)*/),
+          ]),
+        ],
+        author: [
+          thought.author,
+          Validators.compose([Validators.required, Validators.minLength(3)]),
+        ],
+        model: [thought.model],
+      });
+    });
   }
 
-  editThought(){
-    this.thoughtService.edit(this.thought).subscribe(() => this.router.navigate(['/listThoughts']))
+  editThought() {
+    this.thoughtService
+      .edit(this.form.value)
+      .subscribe(() => this.router.navigate(['/listThoughts']));
   }
 
   cancel() {
-    this.router.navigate(['/listThoughts'])
+    this.router.navigate(['/listThoughts']);
   }
 
+  enableButton(): string {
+    if (this.form.valid) {
+      return 'botao';
+    }
+    return 'botao__desabilitado';
+  }
 }
